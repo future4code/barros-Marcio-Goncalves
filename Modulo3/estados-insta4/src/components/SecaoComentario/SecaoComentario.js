e591b7d1a6fe346ce2777d969df52ed4343ad540
Marcio Emmanuel <100731829+M-Emmanuel-G@users.noreply.github.com>
2022-06-28 23:06:21 -0300

    4states

---
+++ Modulo3/estados-insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './style.css'
+
+
+export function SecaoComentario(props) {
+
+	return (
+		<div className='CommentContainer'>
+			<input className='InputComentario'
+				placeholder={'Comentário'}
+				value={props.resposta}
+				onChange={props.onChangeComentario}
+			/>
+			<button onClick={props.aoEnviar}>Enviar</button>
+		</div>
+	)
+}
