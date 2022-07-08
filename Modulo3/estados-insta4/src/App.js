424bdc0d8fc4dbfd41d53d7c5ebd30b6c62a9a33
emmanuel <emmanuel-jnr@hotmail.com>
2022-07-07 23:18:14 -0300

    exercicio

--- Modulo3/estados-insta4/src/App.js
+++ Modulo3/estados-insta4/src/App.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './components/Post/Post';
 import { MainContainer } from "./styledComponents"
 
 function App() {
 
-  const PostList = [
-    {nomeUsuario:'Antonio Nunes',
+  const [ListPost, SetListPost] = useState(
+    {nomeUsuario:'Madruguinha',
     fotoUsuario: "https://picsum.photos/50/49",
     fotoPost:"https://picsum.photos/50/51",
     },
+  )
 
-    {nomeUsuario:'Poderoso Castiga',
-    fotoUsuario: "https://picsum.photos/50/50",
-    fotoPost:"https://picsum.photos/50/52",
-    },
-
-    {nomeUsuario:'Sr Madruga',
-    fotoUsuario: "https://picsum.photos/50/50",
-    fotoPost:"https://picsum.photos/50/53",
+  const PostList = [
+    {nomeUsuario:'Madruguinha',
+    fotoUsuario: "https://picsum.photos/50/49",
+    fotoPost:"https://picsum.photos/50/51",
     },
   ]
 
+
   const PostComponent = PostList.map((valor, index)=>{
     return(
-      <li key={index}><Post nomeUsuario={valor.nomeUsuario} fotoUsuario={valor.fotoUsuario} fotoPost={valor.fotoPost}/></li>
+      <>
+        <li key={index}><Post nomeUsuario={valor.nomeUsuario} fotoUsuario={valor.fotoUsuario} fotoPost={valor.fotoPost}/></li>
+      </>
     )
   })
   console.log(PostList);
+
 return(
-  <MainContainer><ul>{PostComponent}</ul></MainContainer>
+  <>
+    <MainContainer><ul>{PostComponent}</ul></MainContainer>
+  </>
   )
 }
 
