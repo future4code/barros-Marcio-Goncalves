4080bed936101b67836f359b4971a70cc443dc02
Emmanuel <emannuel@MacBook-Pro-de-Marcio.local>
2023-01-19 21:08:02 -0300

    FEAT:Arquitetura de softare 2

---
+++ Modulo6/arquitetura-de-software-2/labeflix/src/data/UserDatabase.ts
@@ -0,0 +1,23 @@
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase extends BaseDatabase {
+  private static TABLE_NAME = "LABEFLIX_USER";
+
+  async create({ id, name, email, password }: any): Promise<void> {
+    await UserDatabase.connection
+      .insert({
+        id,
+        name,
+        email,
+        password,
+      })
+      .into(UserDatabase.TABLE_NAME);
+  }
+
+  showUser = async ():Promise <void>=>{
+    await UserDatabase.connection(UserDatabase.TABLE_NAME)
+    .select()
+  }
+
+
+}
